33cc417602abbc2ff04ae3d2056f7812da992e84
Yash <bhosaley5@gmail.com>
2019-05-01 18:56:59 -0400

    started building the frontend

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,66 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <TicTacToe message1 = m1  message2 ="x"  message3 ="x" @ohno="alertUser"></TicTacToe>
-  </div>
+  <v-app id="inspire" dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      clipped
+      fixed
+      app
+    >
+      <v-list dense>
+        <v-list-tile @click="hello">
+          <v-list-tile-action>
+            <v-icon>dashboard</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Dashboard</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="hello">
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Settings</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    
+    <v-toolbar app fixed clipped-left>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar>
+
+    <v-content>
+      <DatabaseTable/>
+    </v-content>
+    
+    <v-footer app fixed>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue';
-import TicTacToe from './components/TicTacToe.vue';
-export default {
-  name: 'app',
-  components: {
-    TicTacToe
-  },
+import DatabaseTable from './components/DatabaseTable'
 
-}
+  export default {
+    name: 'App',
+    components: {
+        DatabaseTable
+    },
+    data :() =>{
+        return {
+          drawer: false
+        }
+    },
+    props: {
+      source: String
+    },
+    methods:{
+      hello: function() {
+        alert("hello!");
+      }
+    }
+  }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
